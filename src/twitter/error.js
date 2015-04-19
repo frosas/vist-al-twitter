0ab7eeb927953c0d0a12b4d50a7e681f14a749cc
Francesc Rosàs <francescrosasbosque@gmail.com>
2015-04-19 17:43:24 +0100

    "sharing is not permissible for this status" → "You have already retweeted this tweet"

--- src/twitter/error.js
+++ src/twitter/error.js
@@ -1,10 +1,22 @@
 var TwitterError = module.exports = function (twitError) {
     this._twitError = twitError;
-    this.message = twitError.message || twitError.cause.allErrors;
+    this.message = this._getMessage();
 };
 
 TwitterError.prototype = Object.create(Error.prototype);
 
 TwitterError.prototype.isAlreadyRetweetedError = function () {
-    return this._twitError.cause.allErrors.match('sharing is not permissible for this status');    
+    return this._getErrors().some(function (error) {
+        return error.match('You have already retweeted this tweet');
+    });
+};
+
+TwitterError.prototype._getErrors = function () {
+    var errors = this._twitError.allErrors;
+    return typeof errors == 'string' ? [errors] :
+        errors.map(function (error) { return error.message; });
+};
+
+TwitterError.prototype._getMessage = function () {
+    return this._twitError.message || this._getErrors().join(', ') || 'Unknown error';
 };

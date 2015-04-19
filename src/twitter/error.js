e6905e544057c609e14e92e2d58bb76d490d081c
Francesc Rosàs <francescrosasbosque@gmail.com>
2015-04-19 17:57:23 +0100

    Identify error by code, not by message

--- src/twitter/error.js
+++ src/twitter/error.js
@@ -1,3 +1,5 @@
+var ERROR_ALREADY_RETWEETED = 327;
+
 var TwitterError = module.exports = function (twitError) {
     this._twitError = twitError;
     this.message = this._getMessage();
@@ -6,17 +8,17 @@ var TwitterError = module.exports = function (twitError) {
 TwitterError.prototype = Object.create(Error.prototype);
 
 TwitterError.prototype.isAlreadyRetweetedError = function () {
-    return this._getErrors().some(function (error) {
-        return error.match('You have already retweeted this tweet');
+    return this._twitError.allErrors.some(function (error) {
+        return error.code == ERROR_ALREADY_RETWEETED;
     });
 };
 
-TwitterError.prototype._getErrors = function () {
-    var errors = this._twitError.allErrors;
-    return typeof errors == 'string' ? [errors] :
-        errors.map(function (error) { return error.message; });
+TwitterError.prototype._getMessage = function () {
+    return this._twitError.message || this._getErrorsMessage() || 'Unknown error';
 };
 
-TwitterError.prototype._getMessage = function () {
-    return this._twitError.message || this._getErrors().join(', ') || 'Unknown error';
+TwitterError.prototype._getErrorsMessage = function () {
+    return this._twitError.allErrors.
+        map(function (error) { return error.message; }).
+        join(', ');
 };

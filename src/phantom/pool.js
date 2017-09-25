10629efee868afed68c92823335f7729fa87ca00
Francesc Rosas <francescrosasbosque@gmail.com>
2017-09-25 16:43:29 +0200

    Refact Pool into a class

--- src/phantom/pool.js
+++ src/phantom/pool.js
@@ -2,67 +2,69 @@ var Promise = require("bluebird");
 var phridge = require("phridge");
 var debug = require("debug")("phantom:pool");
 
-var Pool = (module.exports = function(max) {
-  this._max = max;
-  this._current = 0;
-  this._queue = [];
-  this._pool = [];
-});
-
-Pool.prototype.spawn = function(callback) {
-  var pool = this;
-  return new Promise(function(resolve, reject) {
-    pool._queue.push({ callback: callback, resolve: resolve, reject: reject });
-    pool._spawnFromQueue();
-  });
-};
-
-Pool.prototype.runOnPage = function(url, callback) {
-  return this.spawn(function(phridgePhantom) {
-    debug("Opening " + url + "...");
-    return phridgePhantom.openPage(url).then(function(phridgePage) {
-      debug("Running on page...");
-      return phridgePage.run(callback);
-    });
-  });
-};
-
-Pool.prototype._spawnFromQueue = function() {
-  var pool = this;
-  if (this._queue.length) {
-    if (this._current == this._max) return;
-    var next = this._queue.shift();
-    this._current++;
-    this._getOrSpawnPhantom()
-      .then(function(phridgePhantom) {
-        return Promise.try(function() {
-          return next.callback(phridgePhantom);
-        }).finally(function() {
-          pool._pool.push(phridgePhantom);
-        });
-      })
-      .finally(function() {
-        pool._current--;
-        pool._spawnFromQueue();
-      })
-      .then(next.resolve, next.reject);
-  } else {
-    this._disposeAll();
+module.exports = class {
+  constructor(max) {
+    this._max = max;
+    this._current = 0;
+    this._queue = [];
+    this._pool = [];
   }
-};
 
-Pool.prototype._getOrSpawnPhantom = function() {
-  return Promise.resolve(this._pool.shift() || this._spawnPhantom());
-};
+  spawn(callback) {
+    return new Promise((resolve, reject) => {
+      this._queue.push({
+        callback: callback,
+        resolve: resolve,
+        reject: reject
+      });
+      this._spawnFromQueue();
+    });
+  }
 
-Pool.prototype._spawnPhantom = function() {
-  debug("Spawning Phantom...");
-  return phridge.spawn({ autoLoadImages: false, diskCacheEnabled: true });
-};
+  runOnPage(url, callback) {
+    return this.spawn(phridgePhantom => {
+      debug("Opening " + url + "...");
+      return phridgePhantom.openPage(url).then(phridgePage => {
+        debug("Running on page...");
+        return phridgePage.run(callback);
+      });
+    });
+  }
 
-Pool.prototype._disposeAll = function() {
-  while (this._pool.length) {
-    debug("Disposing phantom...");
-    this._pool.shift().dispose(); // TODO Handle rejections
+  _spawnFromQueue() {
+    if (this._queue.length) {
+      if (this._current == this._max) return;
+      var next = this._queue.shift();
+      this._current++;
+      this._getOrSpawnPhantom()
+        .then(phridgePhantom => {
+          return Promise.try(() => next.callback(phridgePhantom)).finally(() =>
+            this._pool.push(phridgePhantom)
+          );
+        })
+        .finally(() => {
+          this._current--;
+          this._spawnFromQueue();
+        })
+        .then(next.resolve, next.reject);
+    } else {
+      this._disposeAll();
+    }
+  }
+
+  _getOrSpawnPhantom() {
+    return Promise.resolve(this._pool.shift() || this._spawnPhantom());
+  }
+
+  _spawnPhantom() {
+    debug("Spawning Phantom...");
+    return phridge.spawn({ autoLoadImages: false, diskCacheEnabled: true });
+  }
+
+  _disposeAll() {
+    while (this._pool.length) {
+      debug("Disposing phantom...");
+      this._pool.shift().dispose(); // TODO Handle rejections
+    }
   }
 };

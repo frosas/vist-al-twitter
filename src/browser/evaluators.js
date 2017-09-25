9156bb1262dae49e02c3014ad7920d9d6ae3702d
Francesc Rosas <francescrosasbosque@gmail.com>
2017-09-25 18:16:25 +0200

    Convert browser evaluators to ES6

--- src/browser/evaluators.js
+++ src/browser/evaluators.js
@@ -1,26 +1,17 @@
-/* global document */
+/* global document Set */
 
-exports.getTopicUrlsEvaluator = function() {
-  return Object.keys(
-    [].slice
-      .call(document.querySelectorAll("a[href]"))
-      .map(function(a) {
-        return a.href.match(/([^#]*)/) && RegExp.$1;
-      })
-      .filter(function(url) {
-        return url.match(/:\/\/[^/]+\/vistaltwitter\/.+/);
-      })
-      .reduce(function(set, url) {
-        set[url] = null;
-        return set;
-      }, {})
+exports.getTopicUrlsEvaluator = () => {
+  return Array.from(
+    new Set(
+      Array.from(document.querySelectorAll("a[href]"))
+        .map(a => a.href.match(/([^#]*)/) && RegExp.$1)
+        .filter(url => url.match(/:\/\/[^/]+\/vistaltwitter\/.+/))
+    )
   );
 };
 
-exports.getTopicTweetsEvaluator = function() {
-  return [].slice
-    .call(document.querySelectorAll("#content a[href]"))
-    .map(function(a) {
-      return a.href;
-    });
+exports.getTopicTweetsEvaluator = () => {
+  return Array.from(document.querySelectorAll("#content a[href]")).map(
+    a => a.href
+  );
 };
